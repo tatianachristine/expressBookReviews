f3d116497b4efa30932725a524f9fa90d4e07d8b
Tatiana <144362950+tatianachristine@users.noreply.github.com>
2024-01-10 14:28:20 -0500

    Update to /GETs

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,28 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,10));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const keys = Object.keys(books);
+    let result = keys.filter(author);
+    
+
+    //const result = books.filter(checkAuthor);
+    //function checkAuthor(books) {
+    //    return book = document.getElementById("author").value
+   // }
 });
 
 // Get all books based on title
